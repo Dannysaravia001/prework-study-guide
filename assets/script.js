ec5c3abd59032346a01bfd7f04ed9d944cf49f21
unknown <dannysaravia100@gmail.com>
2023-09-18 00:21:29 -0400

    added javascript function flow to project

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,23 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
-    
-    console.log(topics[x]); 
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
 
-/*
-
-if (topic === 'HTML') {
-    
-    console.log("Let's study HTML!");
-  } 
-  else if (topic === 'CSS') {
-    
-    console.log("Let's study CSS!");
-
-  } else if (topic === 'Git') {
-    
-    console.log("Let's study Git!");
-
-  } else if (topic === 'JavaScript') {
-    
-    console.log("Let's study JavaScript!");
-
-  } else {
-    
-    console.log('Please try again!');
-  }
-
-var x = 0;
-   
-   x < shapes.length;
-
-   x++
-
-for(var x = 0; x <shapes.length; x++) {
-    
-    // code block
+function selectTopics() {
+ if (topics === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (topics === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (topics === 'Git') {
+   console.log("Let's study Git!");
+ } else if (topics === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
 }
-*/
+listTopics()
+selectTopics()
